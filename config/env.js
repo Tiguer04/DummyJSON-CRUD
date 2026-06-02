f741dc0bb8b7230cd909a906897fd2099e2ebd1e
Tiguer04 <trustedx12@gmail.com>
2026-06-02 16:14:00 -0500

    Dockerfile created, docker-compose created, new env var created, new libraries added to the package

--- config/env.js
+++ config/env.js
@@ -4,5 +4,10 @@ dotenv.config();
 
 export const envs = {
     PORT: process.env.PORT || 3000,
-    PUBLIC_PATH: process.env.PUBLIC_PATH || 'public'
+    PUBLIC_PATH: process.env.PUBLIC_PATH || 'public',
+    BDUSER: process.env.BDUSER || 'root',
+    BDPASSWORD: process.env.BDPASSWORD || 'root',
+    BDHOST: process.env.BDHOST || 'db',
+    BDDATABASE: process.env.BDDATABASE || 'products_db',
+    BDPORT: process.env.BDPORT || 3306
 }
