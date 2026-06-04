b03f3e210d0392bc4388c3b0a5cf86e03999c0af
Tiguer04 <trustedx12@gmail.com>
2026-06-04 16:24:15 -0500

    2nd version of login, better view for the client and Swagger implemented.

--- public/login.js
+++ public/login.js
@@ -11,6 +11,26 @@ loginButton.addEventListener('click', () => {
   login(email, password);
 });
 
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+emailInput.addEventListener('click', () =>{
+  emailInput.classList.add('inputhovered');
+  passwordInput.classList.remove('inputhovered');
+});
+
+passwordInput.addEventListener('click', () =>{
+  passwordInput.classList.add('inputhovered');
+  emailInput.classList.remove('inputhovered');
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target !== emailInput && event.target !== passwordInput) {
+    emailInput.classList.remove('inputhovered');
+    passwordInput.classList.remove('inputhovered');
+  }
+});
+
 async function login(email, password) {
 
   const respuesta = await fetch('http://localhost:3000/auth/login', {
@@ -27,6 +47,13 @@ async function login(email, password) {
     
     window.location.href = '/index.html';
   } else {
-    alert(data.message);
+    emailInput.value = '';
+    passwordInput.value = '';
+    
+    Swal.fire({
+      icon: "warning",
+      text: data.message,
+    });
+
   }
 }
